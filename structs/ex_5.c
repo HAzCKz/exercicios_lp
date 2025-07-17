5822f5f98b52a1efd94a51968169a95c43f60d95
Pedro Augusto <82516407+HAzCKz@users.noreply.github.com>
2025-07-17 17:37:07 -0300

    ex_5.c

--- structs/ex_5.c
+++ structs/ex_5.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int dia;
+    int mes;
+    int ano;
+} data;
+
+int data_valida(data d1, data d2)
+{
+    int converter(data d)
+    {
+        int total = d.dia;
+
+        for (int m = 1; m < d.mes; m++)
+        {
+            if (m == 2)
+                total += 28;
+            else if (m == 4 || m == 6 || m == 9 || m == 11)
+                total += 30;
+            else
+                total += 31;
+        }
+
+        total += d.ano * 365;
+
+        return total;
+    }
+
+    int dias1 = converter(d1);
+    int dias2 = converter(d2);
+
+    int diferenca = dias1 - dias2;
+    if (diferenca < 0)
+        diferenca = diferenca -1;
+
+    return diferenca;
+}
+
+int main()
+{
+    data date[2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        scanf("%i", &date[i].dia);
+        scanf("%i", &date[i].mes);
+        scanf("%i", &date[i].ano);
+    }
+
+    int diferenca = data_valida(date[0], date[1]);
+    printf("Dias decorridos entre as duas datas: %d\n", diferenca);
+
+    return 0;
+}
 
